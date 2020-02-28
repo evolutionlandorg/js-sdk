7352e1256c36b13b68bd51d99ac1bb3d564cb446
WoeOm <1015996366@qq.com>
2020-02-28 22:06:03 +0800

    Separate production and dev environments

--- webpack.config.js
+++ webpack.config.js
@@ -3,50 +3,79 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
 const Dotenv = require('dotenv-webpack');
 
 const path = require("path");
-const plugins = [ new Dotenv()];
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+const plugins = [new CleanWebpackPlugin(), new Dotenv()];
 
 if (process.env.ANALYZE) {
     plugins.push(new BundleAnalyzerPlugin());
 }
 
+const ENV = process.env.NODE_ENV || 'development';
+const isProd = ENV === 'production';
 
 
 module.exports = {
-    devtool: "source-map",
+    devtool: isProd ? "source-map" : false,
     entry: "./src/index.js",
-    output: {
+    output: isProd ? {
+        chunkFilename: '[name].[chunkhash:8].js',
+        filename: '[name].[hash:8].js',
+        globalObject: '(typeof self !== \'undefined\' ? self : this)',
+        path: path.join(__dirname, "dist")
+    } : {
         filename: "evolutionland.min.js",
+        globalObject: '(typeof self !== \'undefined\' ? self : this)',
         path: path.join(__dirname, "dist")
     },
-    module: {
-        rules: [
-            {
-                test: /\.(js|mjs)$/,
-                exclude: [/node_modules/],
-                loader: require.resolve("babel-loader"),
-                options: {
-                    babelrc: false,
-                    configFile: false,
-                    compact: false,
-                    presets: [
-                        [
-                            "@babel/env",
-                            {
-                                targets: {
-                                    ie: "11"
-                                },
-                                useBuiltIns: "usage"
-                            }
-                        ]
-                    ],
-                    plugins: [
-                        "@babel/plugin-proposal-class-properties",
-                        "@babel/plugin-transform-runtime",
-
-                    ]
+    watch: isProd,
+    watchOptions: {
+        poll: 1000,
+        aggregateTimeout: 2000,
+        ignored: /node_modules/
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendorOther: {
+                    chunks: 'initial',
+                    enforce: true,
+                    name: 'vendor',
+                    test: /node_modules\//
                 }
             }
-        ]
+        }
+    },
+    module: {
+        rules: [{
+            test: /\.(js|mjs)$/,
+            exclude: [/node_modules/],
+            loader: require.resolve("babel-loader"),
+            options: {
+                babelrc: false,
+                configFile: false,
+                compact: false,
+                presets: [
+                    [
+                        "@babel/env",
+                        {
+                            targets: {
+                                ie: "11"
+                            },
+                            useBuiltIns: "usage"
+                        }
+                    ]
+                ],
+                plugins: [
+                    "@babel/plugin-proposal-class-properties",
+                    "@babel/plugin-transform-runtime",
+
+                ]
+            }
+        }]
     },
     plugins
 };
