9f21e4003dcbbfbd8c1fabcc499d55ed88be7d6d
WoeOm <1015996366@qq.com>
2021-03-08 11:31:27 +0800

    support crab dvm

---
+++ src/evolutionland/crab/env/dev.js
@@ -0,0 +1,56 @@
+export default {
+    CONTRACT: {
+        TOKEN_DEX_BRIDGE: "", // cross land transfer(swapBridge)
+        TOKEN_RING: "0x30086E9730981a473FFD0D5370da1f0eE367c87d",
+        TOKEN_WITHDRAW: "0x2aD13770EC1DdCf11b0e07Ee40E89D2B7e0E1DB2", // takeback
+        TOKEN_WITHDRAW_KTON: "0x59CB18e7C5FeDC8Ecf45E9A0E3b864D5107854CC", // takeback
+        TOKEN_AUCTION: "0xfc9E6A8932776ee1cb837E07ba2826541710Ec47", // ClockAuction
+        TOKEN_LAND: "0x5C5c9cd986a459194500642f69D10330870b8947", //objectOwnership
+        TOKEN_LUCKYBAG: "",
+        TOKEN_REDPACKAGE: "",
+        TOKEN_BANCOR: "", // BancorExchange
+        TOKEN_BANK: "0x64623f5f18b52D742235299c7e3B24a8Fd0490Fd",
+        TOKEN_KTON: "0x0bb0b75aF274b190613F14F1dE614B71c8A912E2",
+        TOKEN_LOTTERY: "0x6fc3Ec9D923dE58bD4cA0Dc353d850F27cF55707", // PointsRewardPool
+        TOKEN_USER_POINTS: "0xc79C63B5659716c1C06F6499Ef286Fc335441Bd7", //UserPoints
+        TOKEN_ROLES_UPDATER: "0xaEA010Ed815C5d7266261837a4a53ECEa5996eD5", // rolesUpdater
+
+        TOKEN_GEN0_APOSTLE: "0x76AB4b3b487313a76360b081AA67758712B51BB2", //Gen0Apostle
+        TOKEN_APOSTLE_TAKE_BACK: "", //TakeBackNFT
+        TOKEN_APOSTLE_CLOCK_AUCTION: "0xe061b4939e9a8e33761f1111Cda29B01e4Eadd5a", //ApostleClockAuction
+        TOKEN_APOSTLE_SIRING_AUCTION: "0xC0daD374Da7331c932386880f328De715A710b66", //SiringClockAuction
+        TOKEN_APOSTLE_BASE: "0x2fd6Eff8b45fDb585119A431C24E87af898A7219", //ApostleBase
+        TOKEN_LAND_RESOURCE: "0xf9E98715170994facfD93332717446D8Af9F8786", //landResource
+        TOKEN_TOKEN_USE: "0x7496C3cfD19eFC7814E999A5f920CD94284bCE65", //token use
+
+        TOKEN_KITTY_CORE: "", // KittyCore
+        TOKEN_PET_BASE: "0xB25E94D6E1C9F18648237B8d5a91848894cbFdC2", // PetBaseProxy
+        TOKEN_GEGO: "", // Dego Token
+
+        TOKEN_ELEMENT_GOLD: "0x9F2a6eE94f71CdB922960E98aC2C346ed81a11d6",
+        TOKEN_ELEMENT_WOOD: "0xeb20555467903C541BA4ea94d943f5412d169fd8",
+        TOKEN_ELEMENT_WATER: "0x1ebcE42690b167E913F086c2F628518d3eCe2f33",
+        TOKEN_ELEMENT_FIRE: "0x03d4cafc8B8a02a319740a9C557DE96fE86267A2",
+        TOKEN_ELEMENT_SOIL: "0xbf4bB4D0fA29cDeDb420EeB33543d7cDF61b78c7",
+
+        PROVIDER: `https://ropsten.infura.io/v3/616e88b770be403a8be78c4b545a5298`,
+
+        SCAN_URL: "https://ropsten.etherscan.io/",
+        SCAN_TRANSACTION: "https://ropsten.etherscan.io/tx",
+        SCAN_ADDRESS: "https://ropsten.etherscan.io/address",
+
+        NETWORK: "43",
+
+        GENESISHOLDER: "0xbc53CD6f0d7dd4fBCf71581E167A0E276f2eD5Bd",
+        UNISWAP_EXCHANGE: "",
+        AUCTIONHOLDER: "",
+
+        FURNACE_TREASURE: "",
+        FURNACE_TAKEBACK: "",
+        FURNACE_ITEMBASE: "",
+
+        ITERING_CALLBACK: "https://alpha.evolution.land/api/eth/raw_sign_broadcast"
+    },
+    DOMAIN: "https://evolution.land.l2me.com",
+    ABI_DOMAIN: "https://devstatic.l2me.com/static"
+};
