4f537ff151637b8ad0546ef162fdd9d7afd156bd
WoeOm <1015996366@qq.com>
2021-07-22 11:46:39 +0800

    fix: update wethGetToken params

--- src/evolutionland/api/weth/index.js
+++ src/evolutionland/api/weth/index.js
@@ -2,8 +2,8 @@ import { Token } from '@uniswap/sdk'
 
 let WethApi = {
 
-  wethGetToken() {
-    return new Token(parseInt(this.env.CONTRACT.NETWORK), this.env.CONTRACT.TOKEN_WETH, 18, 'WHT', "Wrapped HT")
+  wethGetToken(symbol='WEHT', name='Wrapped ether') {
+    return new Token(parseInt(this.env.CONTRACT.NETWORK), this.env.CONTRACT.TOKEN_WETH, 18, symbol, name)
   },
 
 };
