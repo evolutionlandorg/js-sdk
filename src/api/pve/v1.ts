239b85fa3a7a09bd405ed06ac955e8193bf5427b
JayJay1024 <lygm1062@gmail.com>
2021-09-27 19:34:57 +0800

    chore: complete pve api

--- src/api/pve/v1.ts
+++ src/api/pve/v1.ts
@@ -22,6 +22,22 @@ export const pveTeamJoin = async (
 };
 
 /**
+ * PVE Team Joins
+ * @param signer Ethers signer
+ * @param slots Slot
+ * @param tokenIds Apostles token id
+ * @param callback Callback
+ */
+export const pveTeamJoins = async (
+  signer: ethers.Signer,
+  slots: SlotType[],
+  tokenIds: string[],
+  callback?: CallbackType
+) => {
+  await triggerContract(signer, "pveTeam", "joins", [slots, tokenIds.map((tokenId) => utilsPad0x(tokenId))], callback);
+};
+
+/**
  * PVE Team Exit
  * @param signer Ethers signer
  * @param tokenId Apostle token id
@@ -29,6 +45,42 @@ export const pveTeamJoin = async (
  */
 export const pveTeamExit = async (signer: ethers.Signer, tokenId: string, callback?: CallbackType): Promise<void> => {
   await triggerContract(signer, "pveTeam", "exit", [utilsPad0x(tokenId)], callback);
+};
+
+/**
+ * PVE Team Exits
+ * @param signer Ethers signer
+ * @param tokenIds Apostles token id
+ * @param callback Callback
+ */
+export const pveTeamExits = async (signer: ethers.Signer, tokenIds: string[], callback?: CallbackType) => {
+  await triggerContract(signer, "pveTeam", "exits", [tokenIds.map((tokenId) => utilsPad0x(tokenId))], callback);
+};
+
+/**
+ * PVE Team Swap
+ * @param signer Ethers signer
+ * @param slot Slot
+ * @param newTokenId Apostle token id
+ * @param callback Callback
+ */
+export const pveTeamSwap = async (
+  signer: ethers.Signer,
+  slot: SlotType,
+  newTokenId: string,
+  callback?: CallbackType
+) => {
+  await triggerContract(signer, "pveTeam", "swap", [slot, utilsPad0x(newTokenId)], callback);
+};
+
+/**
+ * PVE Team Evict (Force exit after the end of the lease period)
+ * @param signer Ethers signer
+ * @param tokenId Apostle token id
+ * @param callback Callback
+ */
+export const pveTeamEvict = async (signer: ethers.Signer, tokenId: string, callback?: CallbackType) => {
+  await triggerContract(signer, "pveTeam", "evict", [utilsPad0x(tokenId)], callback);
 };
 
 /**
@@ -47,4 +99,22 @@ export const pveTeamAt = async (
 ): Promise<string> => {
   const results = await viewContract(provider, "pveTeam", "at", [user, slot], callback);
   return (results[0] as ethers.BigNumber).toHexString();
+};
+
+/**
+ * PVE Team Exist
+ * @param provider Ethers provider
+ * @param user Account address
+ * @param slot Slot
+ * @param callback Callback
+ * @returns Boolean true or false
+ */
+export const pveTeamExist = async (
+  provider: ethers.providers.Provider,
+  user: string,
+  slot: SlotType,
+  callback?: CallbackType
+): Promise<boolean> => {
+  const results = await viewContract(provider, "pveTeam", "exist", [user, slot], callback);
+  return results[0] as boolean;
 };
