c06019e10809ab8b24c8f4ce3b216691ed8a23d0
JayJay1024 <lygm1062@gmail.com>
2021-09-28 15:13:07 +0800

    chore: complete goldrush api

--- src/api/goldrush/v1.ts
+++ src/api/goldrush/v1.ts
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import { utilsPad0x } from "@evo/utils/index";
 import type { CallbackType } from "@evo/utils/contractHelpers";
-import { triggerContract } from "@evo/utils/contractHelpers";
+import { triggerContract, viewContract } from "@evo/utils/contractHelpers";
 
 /**
  * Goldrush Join
@@ -27,4 +27,132 @@ export const goldRushRaffleJoin = async (
     [eventId, utilsPad0x(landTokenId), amount, subAddr],
     callback
   );
+};
+
+/**
+ * Goldrush Change Amount
+ * @param signer Ethers signer
+ * @param eventId Event id
+ * @param landTokenId Land token id
+ * @param amount Amount (to change)
+ * @param callback Callback
+ */
+export const goldRushRaffleChangeAmount = async (
+  signer: ethers.Signer,
+  eventId: number,
+  landTokenId: string,
+  amount: string,
+  callback?: CallbackType
+) => {
+  await triggerContract(signer, "goldRushRaffle", "changeAmount", [eventId, utilsPad0x(landTokenId), amount], callback);
+};
+
+/**
+ * Goldrush Change Subaddress
+ * @param signer Ethers signer
+ * @param eventId Event id
+ * @param landTokenId Land token id
+ * @param subAddr Sub address (to change)
+ * @param callback Callback
+ */
+export const goldRushRaffleChangeSubAddr = async (
+  signer: ethers.Signer,
+  eventId: number,
+  landTokenId: string,
+  subAddr: string,
+  callback?: CallbackType
+) => {
+  await triggerContract(
+    signer,
+    "goldRushRaffle",
+    "changeSubAddr",
+    [eventId, utilsPad0x(landTokenId), subAddr],
+    callback
+  );
+};
+
+/**
+ * Goldrush Change Info
+ * @param signer Ethers signer
+ * @param eventId Event id
+ * @param landTokenId Land token id
+ * @param amount Amount (to change)
+ * @param subAddr Sub address (to change)
+ * @param callback Callback
+ */
+export const goldRushRaffleChangeInfo = async (
+  signer: ethers.Signer,
+  eventId: number,
+  landTokenId: string,
+  amount: number,
+  subAddr: string,
+  callback?: CallbackType
+) => {
+  await triggerContract(
+    signer,
+    "goldRushRaffle",
+    "change",
+    [eventId, utilsPad0x(landTokenId), amount, subAddr],
+    callback
+  );
+};
+
+/**
+ * Goldrush Exit
+ * @param signer Ethers signer
+ * @param eventId Event id
+ * @param landTokenId Land token id
+ * @param callback Callback
+ */
+export const goldRushRaffleExit = async (
+  signer: ethers.Signer,
+  eventId: number,
+  landTokenId: string,
+  callback?: CallbackType
+) => {
+  await triggerContract(signer, "goldRushRaffle", "exit", [eventId, utilsPad0x(landTokenId)], callback);
+};
+
+/**
+ * Goldrush Draw
+ * @param signer Ethers signer
+ * @param eventId Event id
+ * @param landTokenId Land token id
+ * @param isWon Is won or not
+ * @param param4 { hashmessage, v, r, s }
+ * @param callback Callback
+ */
+export const goldRushRaffleDraw = async (
+  signer: ethers.Signer,
+  eventId: number,
+  landTokenId: string,
+  isWon: boolean,
+  { hashmessage, v, r, s }: { hashmessage: string; v: number; r: string; s: string },
+  callback?: CallbackType
+) => {
+  await triggerContract(
+    signer,
+    "goldRushRaffle",
+    "draw",
+    [eventId, utilsPad0x(landTokenId), isWon, hashmessage, v, r, s],
+    callback
+  );
+};
+
+/**
+ * Goldrush History
+ * @param provider Ethers provider
+ * @param eventId Event id
+ * @param landTokenId Land token id
+ * @param callback Callback
+ * @returns String balance or '0'
+ */
+export const goldRushRaffleGetHistory = async (
+  provider: ethers.providers.Provider,
+  eventId: number,
+  landTokenId: string,
+  callback?: CallbackType
+): Promise<string> => {
+  const results = await viewContract(provider, "goldRushRaffle", "lands", [eventId, utilsPad0x(landTokenId)], callback);
+  return (results.balance as ethers.BigNumber).toString();
 };
